6bb947fae8024ae336348b8077d0b80a7051beaa
DreamPerson <dreamPerson0106@gmail.com>
2023-09-14 23:29:47 +0900

    asdf

--- hardhat.config.js
+++ hardhat.config.js
@@ -41,6 +41,15 @@ module.exports = {
           },
         },
       },
+      {
+        version: "0.6.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200,
+          },
+        },
+      },
     ],
   }
 };
